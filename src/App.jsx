41629bc1b0275fe593a35a88e79466ca95e8b804
YusrohAdebukola <112622032+YusrohAdebukola@users.noreply.github.com>
2026-06-07 18:02:54 +0100

    Create App component with AnalyticsDashboard

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AnalyticsDashboard from './AnalyticsDashboard';
+
+function App() {
+  return (
+    <div>
+      <h1>Apvertise</h1>
+      <AnalyticsDashboard />
+    </div>
+  );
+}
+
+export default App;
